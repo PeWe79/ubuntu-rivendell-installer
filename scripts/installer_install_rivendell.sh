f6017ffe6e291fb0c7a16560a58e5d3aaff8baed
Fred Gleason <fredg@paravelsystems.com>
2024-05-02 17:05:55 -0400

    2024-05-02 Fred Gleason <fredg@paravelsystems.com> 	* Added 'paravelview' and 'lwmon' packages to the base installation.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- scripts/installer_install_rivendell.sh
+++ scripts/installer_install_rivendell.sh
@@ -95,7 +95,7 @@ echo $NFS_MOUNT_TYPE >> /root/rivendell_install_log.txt
 #
 # Install Dependencies
 #
-apt -y install openssh-server patch evince telnet samba chrony emacs nfs-common smbclient net-tools traceroute gedit ntfs-3g autofs libmad0 libtwolame0 libmp3lame0
+apt -y install openssh-server patch evince telnet samba chrony emacs nfs-common smbclient net-tools traceroute gedit ntfs-3g autofs libmad0 libtwolame0 libmp3lame0 paravelview lwmon
 
 if test $MODE = "server" ; then
     #
